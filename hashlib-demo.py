1f2bfa1e725ec39951486679309f16a3043a6236
Neilai <1131894367@qq.com>
2018-01-31 14:35:17 +0800

    add hashlib demo

---
+++ hashlib-demo.py
@@ -0,0 +1,15 @@
+__author__ = "Neil"
+__time__ = "2018/1/31 14:25"
+
+import hashlib
+
+# md5 = hashlib.md5()
+# md5.update(b'how to use md5 in python hashlib?')
+# print(md5.hexdigest())
+
+# print(hashlib.sha224(b"Nobody inspects the spammish repetition").hexdigest())
+
+##自定义算法
+# h = hashlib.new('ripemd160')
+# h.update(b"Nobody inspects the spammish repetition")
+# h.hexdigest()
